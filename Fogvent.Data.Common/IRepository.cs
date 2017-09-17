b6cf97d69231bde7cad05522121fb5e42ad6c699
tarekselem <t.hamdy91@yahoo.com>
2017-09-17 10:01:56 +0200

    Solution Structure

---
+++ Fogvent.Data.Common/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fogvent.Data.Common
+{
+    public interface IRepository<T>
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> Find(Expression<Func<T, bool>> where);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(object id);
+        void Attach(T entity);
+
+    }
+}
---
+++ Fogvent.Data.SQL/Repository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using Fogvent.Data.Common;
+
+namespace Fogvent.Data.SQL
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        #region MyRegion
+
+        
+
+        #endregion
+        private DbSet<T> _entitySet;
+
+        public Repository(DbContext context)
+        {
+            if (context == null) throw new Exception("Context cannot be null");
+
+            _entitySet = context.Set<T>();
+        }
+
+
+
+        public void Attach(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> Find(Expression<Func<T, bool>> where)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
