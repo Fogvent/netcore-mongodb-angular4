1cfe0ae9477528008d621b38f67cdac0541b978e
tarekselem <t.hamdy91@yahoo.com>
2017-09-30 23:43:37 +0200

    Cross Cuting Init

---
+++ Fogvent.CrossCutting/Modules.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fogvent.Core.Extensions;
+using Ninject.Modules;
+
+namespace Fogvent.CrossCutting
+{
+    public class Modules:NinjectModule
+    {
+        public override void Load()
+        {
+            var SelectedProvider = "DefaultProvider".GetConfigurationValue();
+            Bind<>()
+        }
+    }
+}
