efd1b49e8ddf6227c9d507d1cc02d86a87bcfd77
tarekselem <t.hamdy91@yahoo.com>
2017-10-06 16:25:41 +0200

    Restructure Services

---
+++ Fogvent.Core/ILogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fogvent.Core
+{
+    public interface ILogger
+    {
+        void LogDebug(string message);
+        void LogWarning(string message);
+        void LogInformation(string message);
+        void LogError(Exception exception);
+    }
+}
