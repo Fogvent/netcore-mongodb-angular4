7b484d0492599e760c726de96518f08e222c0fbe
tarekselem <t.hamdy91@yahoo.com>
2018-04-25 19:30:22 +0200

    old code

---
+++ Fogvent.Plugins.Tests/LoggerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fogvent.Plugins.Tests
+{
+    [TestClass]
+    public class LoggerTests
+    {
+        [TestMethod]
+        public void TestLogDebug()
+        {
+        }
+
+        [TestMethod]
+        public void TestLogWarning()
+        {
+        }
+
+        [TestMethod]
+        public void TestLogInformation()
+        {
+        }
+
+        [TestMethod]
+        public void TestLogError()
+        {
+        }
+
+    }
+}
---
+++ Fogvent.Plugins.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Fogvent.Plugins.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Fogvent.Plugins.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("165c618a-ed0d-461b-9d8c-d7434ef5991b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+//[assembly: log4net.Config.XmlConfigurator(Watch = true)]
